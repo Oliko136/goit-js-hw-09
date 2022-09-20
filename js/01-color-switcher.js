0568610339e177780e2e4b448afb611909a1dd2c
Oliko136 <olchick136@gmail.com>
2022-09-21 00:14:16 +0400

    Task 1 completed

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    onInterval();
+}
+
+function onStop() {
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+
+    clearInterval(timerId);
+}
+
+function onInterval() {
+        document.body.style.backgroundColor = getRandomHexColor();
+        
+        timerId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000)
+    };
